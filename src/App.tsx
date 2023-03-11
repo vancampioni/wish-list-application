ecceaf94b14720ff8ae83e240d7500a2198b88c5
vancampioni <vanessacrtsilva@gmail.com>
2023-03-11 12:03:58 -0300

    feat: building Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import Header from './components/Header'
+
 const App = () => {
 
   return (
-    <div>
-      <h1>App</h1>
+    <>
+      <Header />
       <Outlet />
-    </div>
+    </>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import { BsSearch } from 'react-icons/bs'
 
 const Header = () => {
 
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <>
+      <div>
+        <div>
+          <h1>MagaNets</h1>
+        </div>
+        <div>
+          <p>Cidade: São Paulo</p>
+        </div>
+        <div>
+          <span>Central de atendimento</span>
+        </div>
+        <div>
+          <p>Lista de desejos</p>
+        </div>
+      </div>
+      <div>
+        <form autoComplete='off'>
+          <button><BsSearch /></button>
+          <input type='search' placeholder='Busca'/>
+        </form>
+      </div>
+    </>
   )
 }
 
